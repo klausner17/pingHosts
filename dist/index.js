43ca364b9b87d742ee54d8d30764a5d62c9b5f11
Klausner Augusto <klausner17@gmail.com>
2017-08-03 00:44:20 -0300

    Removendo dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PingHosts_1 = require("./PingHosts");
-const HOSTS = require('./data/hosts.json');
-var pingHosts = new PingHosts_1.default(HOSTS['hosts']);
-pingHosts.pingAllHosts();
